6604f304ab16d9038ccc1cde4a3f75ddda0382ca
Scott Daniel <sadaniel35@gmail.com>
2023-12-14 15:32:44 -0800

    Add PokemonCell

---
+++ Pokedex/Pokedex/Views/PokemonCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokemonCell.swift
+//  Pokedex
+//
+//  Created by Scott Daniel on 12/14/23.
+//
+
+import SwiftUI
+
+struct PokemonCell: View 
+{
+    var body: some View
+    {
+        ZStack {
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+        }
+        .background(.green)
+    }
+}
+
+#Preview {
+    PokemonCell()
+}
