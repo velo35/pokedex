5fbf29541d51f1fed3bd66eca514da5e90b8ab3c
Scott Daniel <sadaniel35@gmail.com>
2023-12-14 15:28:03 -0800

    Initial check-in

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Scott Daniel on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
