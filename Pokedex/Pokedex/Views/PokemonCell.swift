17bcc1e458b6e6d59b19ae986bd097d018c23100
Scott Daniel <sadaniel35@gmail.com>
2023-12-14 16:15:55 -0800

    Finished PokemonCell

--- Pokedex/Pokedex/Views/PokemonCell.swift
+++ Pokedex/Pokedex/Views/PokemonCell.swift
@@ -12,11 +12,37 @@ struct PokemonCell: View
     var body: some View
     {
         ZStack {
-            VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                Text("Bulbasaur")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .padding(.top, 8)
+                    .padding(.leading)
+                
+                HStack {
+                    Text("poison")
+                        .font(.subheadline.bold())
+                        .foregroundStyle(.white)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .overlay {
+//                            RoundedRectangle(cornerRadius: 20)
+                            Capsule()
+                                .fill(Color.white.opacity(0.25))
+                        }
+                        .frame(width: 100, height: 24)
+                    
+                    Image("bulbasaur")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 68, height: 68)
+                        .padding([.bottom, .trailing], 4)
+                }
             }
         }
         .background(.green)
+        .clipShape(.rect(cornerRadius: 12))
+        .shadow(color: .green, radius: 6)
     }
 }
 
