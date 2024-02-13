98feddc9139ec27b92d9559459868e3532c12b07
Scott Daniel <sadaniel35@gmail.com>
2024-02-12 16:25:19 -0800

    Adding SelectionAlignment and selectionAlignmentGuide to OptionsPanel

--- Pokedex/Pokedex/Utils/AlignmentExtensions.swift
+++ Pokedex/Pokedex/Utils/AlignmentExtensions.swift
@@ -5,4 +5,16 @@
 //  Created by Scott Daniel on 2/12/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension VerticalAlignment
+{
+    private struct SelectionAlignment: AlignmentID
+    {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[VerticalAlignment.center]
+        }
+    }
+    
+    static let selectionAlignmentGuide = VerticalAlignment(SelectionAlignment.self)
+}
--- Pokedex/Pokedex/Views/Components/OptionsPanel.swift
+++ Pokedex/Pokedex/Views/Components/OptionsPanel.swift
@@ -13,10 +13,37 @@ struct OptionsPanel: View
     
     var body: some View
     {
-        VStack(spacing: 16) {
-            ForEach(UIMode.allCases) { uiMode in
-                Button(uiMode.rawValue) {
-                    mode = uiMode
+        VStack {
+            Text("UI Mode")
+                .font(.largeTitle)
+            
+            Spacer()
+                .frame(height: 20)
+            
+            ZStack(alignment: Alignment(horizontal: .center, vertical: .selectionAlignmentGuide)) {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.purple.opacity(0.5))
+                    .frame(width: 120, height: 40)
+                    .alignmentGuide(.selectionAlignmentGuide) { d in
+                        d[VerticalAlignment.center]
+                    }
+                
+                
+                VStack(spacing: 16) {
+                    ForEach(UIMode.allCases) { uiMode in
+                        Button {
+                            withAnimation(.easeInOut(duration: 0.25)) {
+                                mode = uiMode
+                            }
+                        } label: {
+                            Text(uiMode.rawValue)
+                                .font(.title.weight(.semibold))
+                                .foregroundStyle(.black)
+                        }
+                        .alignmentGuide(mode == uiMode ? .selectionAlignmentGuide : VerticalAlignment.center) { d in
+                            d[VerticalAlignment.center]
+                        }
+                    }
                 }
             }
         }
