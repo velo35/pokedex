c3c953d59254f7915645817a4b86e7faeaac230c
Scott Daniel <sadaniel35@gmail.com>
2024-02-28 14:20:13 -0800

    Refactor PokemonType extensions into separate file PokemonTypeExtensions

---
+++ Pokedex/Pokedex/Extensions/PokemonTypeExtensions.swift
@@ -0,0 +1,48 @@
+//
+//  PokemonTypeExtensions.swift
+//  Pokedex
+//
+//  Created by Scott Daniel on 2/28/24.
+//
+
+import SwiftUI
+
+extension PokemonType
+{
+    var color: Color
+    {
+        switch self {
+            case .normal: .orange
+            case .fighting: .orange
+            case .flying: .teal
+            case .poison: .green
+            case .ground: .gray
+            case .rock: .brown
+            case .bug: .mint
+            case .ghost: .gray
+            case .steel: .gray
+            case .fire: .red
+            case .water: .blue
+            case .grass: .green
+            case .electric: .yellow
+            case .psychic: .purple
+            case .ice: .cyan
+            case .dragon: .orange
+            case .dark: .gray
+            case .fairy: .pink
+            case .unknown: .orange
+            case .shadow: .gray
+        }
+    }
+    
+    var image: Image
+    {
+        switch self {
+            case .fire: Image(systemName: "flame.circle.fill")
+            case .grass: Image(systemName: "leaf.circle.fill")
+            case .water: Image(systemName: "drop.circle.fill")
+            case .electric: Image(systemName: "bolt.circle.fill")
+            default: Image(systemName: "questionmark")
+        }
+    }
+}
--- Pokedex/Pokedex/Models/PokemonType.swift
+++ Pokedex/Pokedex/Models/PokemonType.swift
@@ -5,7 +5,7 @@
 //  Created by Scott Daniel on 2/12/24.
 //
 
-import SwiftUI
+import Foundation
 
 enum PokemonType: String, Identifiable
 {
@@ -30,36 +30,5 @@ enum PokemonType: String, Identifiable
     case unknown
     case shadow
     
-    var id: PokemonType { self }
-}
-
-extension PokemonType
-{
-    var color: Color
-    {
-        switch self {
-            case .fire: .red
-            case .grass: .green
-            case .bug: .mint
-            case .water: .blue
-            case .electric: .yellow
-            case .psychic: .purple
-            case .normal: .orange
-            case .ground: .gray
-            case .flying: .teal
-            case .fairy: .pink
-            default: .black
-        }
-    }
-    
-    var image: Image
-    {
-        switch self {
-            case .fire: Image(systemName: "flame.circle.fill")
-            case .grass: Image(systemName: "leaf.circle.fill")
-            case .water: Image(systemName: "drop.circle.fill")
-            case .electric: Image(systemName: "bolt.circle.fill")
-            default: Image(systemName: "questionmark")
-        }
-    }
+    var id: Self { self }
 }
